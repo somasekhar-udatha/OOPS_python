9861d895a5564e825be95a31db8a1808ca14d47f
Soma Sekhar Udatha <115133440+somasekhar-udatha@users.noreply.github.com>
2024-07-16 15:46:58 +0530

    Create init_method.py

---
+++ init_method.py
@@ -0,0 +1,9 @@
+class Computer:
+    def __init__(self):
+        print("in init method")
+    
+    def config(self):
+        print("config is: ",self.cpu,self.ram)
+comp = Computer()
+
+# o/p: in init method
